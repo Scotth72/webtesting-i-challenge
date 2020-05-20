d6707e5af0c9234b1a92bd7d2622389549adc495
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-20 18:04:38 -0500

    Initial commit

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,27 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	if (item.enhancement < 20) {
+		item.enhancement = item.enhancement + 1;
+		return { ...item };
+	} else {
+		return { ...item };
+	}
 }
 
 function fail(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
